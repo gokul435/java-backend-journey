1a1be8dc21838f648938778229b03db034e8fbc3
gokul435 <gokulakannan29dec@gmail.com>
2026-06-27 09:55:01 +0530

    Week4 - Simple GET using Postman
    @RestController - class handles http's
    @RequestMapping("/api") - This class starts from api
    @GetMapping("/hello") - This method will happen when someone tries GET /api/hello and gives the output in postman

---
+++ week4/src/main/java/org/example/week4/Week4Application.java
@@ -0,0 +1,14 @@
+package org.example.week4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week4Application.class, args);
+		System.out.println("Hello SpringBoot");
+	}
+
+}
